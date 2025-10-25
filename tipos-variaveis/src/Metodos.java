9fd401456c9398f4d7c0d1618ee3020d0b75e77a
Lucas Miranda <mirandalucasemanuel@gmail.com>
2025-10-25 17:19:27 -0300

    Exemplos de métodos

--- tipos-variaveis/src/Metodos.java
+++ tipos-variaveis/src/Metodos.java
@@ -1,7 +1,30 @@
 public class Metodos {
-
-    public static void main (String[] Args) {
+    public static void main(String[] args) {
         
     }
 
+    public double somar(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public void imprimir(String texto) {
+        System.out.println(texto);
+    }
+
+     public double dividir(int num1, int num2) throws Exception {
+        if (num2 == 0) {
+            throw new Exception("Divisão por zero não é permitida!");
+        }
+        return (double) num1 / num2;
+    }
+
+    public boolean validar() {
+        // exemplo validação
+        return true; 
+    }
+
+    public void gravarCliente(String nome, String cpf) {
+        System.out.println("Cliente gravado: " + nome + " - CPF: " + cpf);
+    }
+    
 }
