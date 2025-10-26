8f0920d1676f4a2e6d86451364da48fe36f8332d
miirandadev <mirandalucasemanuel@gmail.com>
2025-10-26 14:17:50 -0300

    Exemplos usando classes e métodos

---
+++ src/Usuario.java
@@ -0,0 +1,31 @@
+public class Usuario {
+    public static void main(String[] args) {
+        
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV ligada ? " + smartTv.ligada);
+        System.out.println("Volume Atual : " + smartTv.volume);
+        System.out.println("Canal Atual : " + smartTv.canal);
+        
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        System.out.println("Novo Status -> Volume: " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo Status -> TV Ligada ? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo Status -> TV Ligada ? " + smartTv.ligada);
+
+        smartTv.mudarDeCanal(13);
+        System.out.println("Canal Atual : " + smartTv.canal);
+
+        smartTv.aumentarCanal();
+
+        System.out.println("Canal Atual : " + smartTv.canal);
+
+    }
+}
